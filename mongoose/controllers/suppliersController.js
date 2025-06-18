acd9dad2c51510091cc7bfc88ff8c9ef23b504b5
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-06-18 14:33:55 +0100

    Update suppliersController.js

--- mongoose/controllers/suppliersController.js
+++ mongoose/controllers/suppliersController.js
@@ -44,3 +44,42 @@ exports.viewSupplier = async (req, res, next) => {
     next(error);
   }
 };
+
+
+exports.renderChangeSupplierForm = async (req, res, next) => {
+  try {
+    const supplier = await mdb.supplier.findOne({ uuid: req.params.uuid }).lean();
+    if (!supplier) {
+      req.flash('error', 'Supplier not found.');
+      return res.redirect('/suppliers');
+    }
+    res.render(path.join('mongoose', 'updateSupplier'), {
+      title: 'Change Supplier',
+      supplier
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.changeSupplier = async (req, res, next) => {
+  try {
+    const { subcontractor, cisRate, cisNumber } = req.body;
+
+    await mdb.supplier.updateOne(
+      { uuid: req.params.uuid },
+      {
+        $set: {
+          Subcontractor: !!subcontractor,
+          IsSubcontractor: !!subcontractor,
+          CISRate: cisRate,
+          CISNumber: cisNumber || null
+        }
+      }
+    );
+
+    return res.redirect('/suppliers');
+  } catch (error) {
+    next(error);
+  }
+};
