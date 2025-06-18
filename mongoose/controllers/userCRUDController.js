8b0bbd5c94c60439853198e6efc3be53afc7aac6
CappyTech <cappytech+github@proton.me>
2025-06-18 17:23:41 +0100

    Add user listing

--- mongoose/controllers/userCRUDController.js
+++ mongoose/controllers/userCRUDController.js
@@ -1,5 +1,29 @@
 const path = require('path');
 const mdb = require('../services/mongooseDatabaseService');
+const moment = require('moment');
+
+exports.listUsers = async (req, res, next) => {
+  try {
+    const users = await mdb.user.find().sort({ createdAt: -1 }).lean();
+    const totalUsers = users.length;
+    const roleCounts = users.reduce((acc, u) => {
+      acc[u.role] = (acc[u.role] || 0) + 1;
+      return acc;
+    }, {});
+    const recentUsers = users.filter(u =>
+      u.createdAt && moment(u.createdAt).isAfter(moment().subtract(30, 'days'))
+    );
+    res.render(path.join('mongoose', 'user'), {
+      title: 'Users',
+      users,
+      totalUsers,
+      roleCounts,
+      recentUsersCount: recentUsers.length,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 exports.createUser = async (req, res, next) => {
   try {
--- mongoose/routes/userCrud.js
+++ mongoose/routes/userCrud.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authService = require('../../services/authService');
 const ctrl = require('../controllers/userCRUDController');
 
+router.get('/dashboard/user', authService.ensureRole(), ctrl.listUsers);
 router.post('/user/create', authService.ensureRole(), ctrl.createUser);
 router.get('/user/read/:uuid', authService.ensureRole(), ctrl.readUser);
 router.post('/user/update/:uuid', authService.ensureRole(), ctrl.updateUser);
