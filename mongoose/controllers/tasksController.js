7e05eb8cb50569b77548991fa7d7d792e962868a
CappyTech <cappytech+github@proton.me>
2025-06-18 16:37:32 +0100

    Add task creation UI and routes

---
+++ mongoose/controllers/tasksController.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const taskService = require('../../services/mongoose/taskService');
+
+exports.renderCreateTaskForm = (req, res) => {
+  res.render(path.join('mongoose', 'createTask'), {
+    title: 'Create Task'
+  });
+};
+
+exports.createTask = async (req, res, next) => {
+  try {
+    const { title, description, dueDate, recurrence } = req.body;
+    const userId = req.user._id;
+    await taskService.createTask({
+      title,
+      description,
+      userId,
+      dueDate: dueDate || null,
+      recurrence: recurrence || 'none'
+    });
+    req.flash('success', 'Task created');
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+};
