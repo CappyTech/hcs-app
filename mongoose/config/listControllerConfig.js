af9eb853727fc942851f2145c92268973079bc99
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-07-01 18:27:16 +0100

    Update listControllerConfig.js

--- mongoose/config/listControllerConfig.js
+++ mongoose/config/listControllerConfig.js
@@ -1,3 +1,4 @@
+const { totp } = require("speakeasy");
 const attendance = require("../models/mongoose/attendance");
 
 module.exports = {
@@ -28,5 +29,17 @@ module.exports = {
     linkField: 'date',
     hideFields: ['__v', '_id','createdAt', 'updatedAt','uuid'],
     fieldOrder: ['date', 'type', 'employeeId', 'subcontractorId', 'hoursWorked', 'payRate', 'dayRate', 'locationId', 'projectId'],
+  },
+  supplier: {
+    title: 'Suppliers',
+    linkField: 'Name',
+    hideFields: ['__v', '_id','createdAt', 'updatedAt','uuid', 'Created', 'Updated'],
+    fieldOrder: ['Name', 'Email', 'Mobile', 'Address1', 'Address2', 'Address3', 'Address4', 'PostCode', 'Telephone', 'Website', 'Contact', 'Mobile', 'Fax', 'CurrencyID', 'PaymentTerms', 'ContactTitle', 'ContactFirstName', 'ContactLastName', 'TradeBorderType', 'IsSubcontractor', 'Subcontractor', 'CISRate', 'CISNumber'],
+  },
+  user: {
+    title: 'Users',
+    linkField: 'username',
+    hideFields: ['__v', '_id','createdAt', 'updatedAt','uuid', 'password', 'totpSecret', 'totpEnabled'],
+    fieldOrder: ['username', 'email', 'role', 'status', 'lastLogin'],
   },
 };
