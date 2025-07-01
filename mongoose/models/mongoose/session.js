3b3114a86d602e1f3883d586b351666fe5023d3b
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-07-01 19:08:58 +0100

    Update session.js

--- mongoose/models/mongoose/session.js
+++ mongoose/models/mongoose/session.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const sessionSchema = new mongoose.Schema({
-  uuid: { type: String, unique: true, required: true, default: uuidv4 },
   _id: { type: String },
   expires: { type: Date, required: true },
   session: { type: mongoose.Schema.Types.Mixed, required: true }
