c5246f2797a47c63b09eb1b5dffdb939444bfa63
CappyTech <133281784+CappyTech@users.noreply.github.com>
2026-03-17 17:01:58 +0000

    Create adminRoutes.js

---
+++ mongoose/routes/adminRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const authService = require('../../services/authService');
+const ctrl = require('../controllers/adminController');
+
+router.get('/admin/deleted-items', authService.ensureRole('admin'), ctrl.getDeletedItems);
+
+module.exports = router;
